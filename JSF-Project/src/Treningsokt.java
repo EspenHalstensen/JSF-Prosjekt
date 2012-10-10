9a69be91d1b2bd81a0f3105bc009f7ae7cd2d394
Amund E. Aarø <amundea@tihlde.org>
2012-10-10 13:44:46 +0200

    Treningsokt

---
+++ JSF-Project/src/Treningsokt.java
@@ -0,0 +1,11 @@
+/**
+ *
+ * @author Amund
+ */
+public class Treningsokt {
+    private int oktnr;
+    private String dato;
+    private int varighet;
+    private String[] kategori = {"styrke", "kondis", "gaming", "ALL the things!"};
+    private String tekst;
+}
